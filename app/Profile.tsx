087156d81c5a6a9a5e4ded8bfc299d1cab9ec8fd
aclaregr <aclaregr@uoguelph.ca>
2025-07-07 00:10:35 -0400

    added logout and saved logins

--- app/Profile.tsx
+++ app/Profile.tsx
@@ -1,11 +1,37 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "expo-router";
 
 export default function Profile() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Logout error:", error.message);
+      return;
+    }
+
+    router.replace("/Start");
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.content}></View>
+      <ScrollView>
+        <View style={styles.content}></View>
+        <TouchableOpacity style={styles.button} onPress={() => handleLogout()}>
+          <Text style={styles.buttonText}>Log out</Text>
+        </TouchableOpacity>
+        <View></View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -16,8 +42,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#222222",
   },
   content: {
-    flex: 1,
+    backgroundColor: "#333",
+    padding: "80%",
+  },
+  button: {
+    backgroundColor: "blue",
     justifyContent: "center",
     alignItems: "center",
+    paddingVertical: "10%",
+  },
+  buttonText: {
+    color: "white",
   },
 });
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,29 @@
 import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
 
 export default function Index() {
-  return <Redirect href="/Start" />;
+  const [initialRoute, setInitialRoute] = useState<"/Start" | "/Home" | null>(
+    null
+  );
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      if (data.session) {
+        setInitialRoute("/Home");
+      } else {
+        setInitialRoute("/Start");
+      }
+      supabase.auth.onAuthStateChange((_event, session) => {
+        setInitialRoute(session ? "/Home" : "/Start");
+      });
+    };
+
+    checkSession();
+  }, []);
+
+  if (!initialRoute) return null;
+
+  return <Redirect href={initialRoute} />;
 }
