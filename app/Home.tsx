a5a50ab99fb74c6edac522c718836e551fce1d22
Avery McLean <mcleanavery04@gmail.com>
2025-07-09 20:27:27 -0400

    Very minor, mostly messing around.

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <View style={styles.content}></View>
+      <View style={styles.content}>
+        <Text style={{ color: "white" }}>Hey cutie</Text>
+      </View>
     </View>
   );
 }
