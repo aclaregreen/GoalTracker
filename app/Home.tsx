d8002e007c940729e8bebcbfc0f7fd763ffecb86
Avery McLean <mcleanavery04@gmail.com>
2025-07-31 20:42:53 -0400

    Added cards to home page.

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import ProgressBar from 'react-native-progress-bar-horizontal';
 
 export default function Home() {
+  const goalsCompleted = 13;
+  const goalsTotal = 16;
+  const progressPercentage = goalsCompleted / goalsTotal;
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-        <Text style={{ color: "white" }}>Hey cutie</Text>
+        <Text style={{ color: "white", marginBottom: 8 }}>
+          Weekly Goals: {goalsCompleted}/{goalsTotal}
+        </Text>
+        <ProgressBar
+          progress={progressPercentage}
+          borderWidth={1}
+          fillColor="#00ff1a"
+          unfilledColor="#085200ff"
+          height={20}
+          borderColor="#00ff1a"
+          duration={100}
+          width={350}
+          borderRadius={15}
+        />
+      </View>
+
+      {/* Cards fill the rest of the space */}
+      <View style={styles.cards}>
+        <View style={[styles.card, { flex: 1, marginTop: 20 }]} />
+        <View style={[styles.card, { flex: 1 }]} />
+        <View style={[styles.card, { flex: 0.8 }]} />
       </View>
     </View>
   );
@@ -17,8 +42,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#222222",
   },
   content: {
-    flex: 1,
-    justifyContent: "center",
+    paddingTop: 20,
+    paddingBottom: 10,
     alignItems: "center",
+  },
+  cards: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: "space-between",
+  },
+  card: {
+    backgroundColor: "#3c3c3c", // gray like your Figma
+    borderRadius: 12,
+    marginBottom: 20,
   },
 });
